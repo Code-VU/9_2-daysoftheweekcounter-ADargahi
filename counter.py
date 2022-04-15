775b09610eafe0c432bb7af68fe1bad84958ef5c
Abbas Dargahi <adargahi97@gmail.com>
2022-04-15 12:57:21 -0500

    assignment 9.2

--- counter.py
+++ counter.py
@@ -1,7 +1,23 @@
 
 def countDayOfTheWeek():
     # This first line is provided for you
-    file_name = input("Enter a file name: ")    # end assignment
+    file_name = input("Enter a file name: ")
+    openFile = open(file_name)
+    file = openFile.readlines()
+    fromDict = {}
+
+    for line in file:
+        if line.startswith("From "):
+            line = line.strip()
+            line = line.split(' ')
+            if line[2] in fromDict:
+                fromDict[line[2]] += 1
+            else:
+                fromDict[line[1]] = 1
+    print(fromDict)
+
+
+            
 
 ## if you want to test locally before you try to sync
 ## uncomment calculateAbsolute() and run > python payCalculator.py
